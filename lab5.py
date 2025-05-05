b8953218f4ddfe2f4e7bb17a93be6c41bbcc4a83
unknown <gianenabore30@gmail.com>
2025-05-05 23:39:50 +0800

    implementing derived class of animal

--- lab5.py
+++ lab5.py
@@ -9,3 +9,24 @@ class Animal:
     def describe(self):
         print(f"Animal named {self.name}, and Age: {self.age}")
 
+def Lion(Animal):
+    def make_sound(self):
+        print("RAWR RAWR RAW!!!")
+    
+    def roar(self):
+        print("ROARS Loudly.")
+
+def Bird(Animal):
+    def make_sound(self):
+        print("CHIRP CHIRP CHIRP!!!")
+    
+    def fly(self):
+        self("Flies high in the sky")
+
+def Reptile(Animal):
+    def make_sound(self):
+        print("Hiss HIss HIss!!")
+    
+    def crawl(self):
+        print("crawls silently, gumagapang skibidi.")
+
