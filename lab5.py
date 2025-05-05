f211852658ff8a9cbb1e491797e7dc30202be73e
unknown <gianenabore30@gmail.com>
2025-05-05 23:33:02 +0800

    initiate base class

--- lab5.py
+++ lab5.py
@@ -1,1 +1,11 @@
-print('Hello Lab5!')
+class Animal:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def make_sound(self):
+        print("Animal's sound.")
+    
+    def describe(self):
+        print(f"Animal named {self.name}, and Age: {self.age}")
+
